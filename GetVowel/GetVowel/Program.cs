72632be28fed8a73b9a57f681e399539f9c09ce0
spencerj <vorpehd@gmail.com>
2014-09-18 19:53:17 -0400

    Added Proj.
    asdfgstg

---
+++ GetVowel/GetVowel/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GetVowel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter A Vowel: ");
+            string Vowel = Console.ReadLine();
+            switch (Vowel)
+            {
+                case "a":
+                    Console.WriteLine("OK");
+                    break;
+                case "A":
+                    Console.WriteLine("OK");
+                    break;
+                case "e":
+                    Console.WriteLine("OK");
+                    break;
+                case "E":
+                    Console.WriteLine("OK");
+                    break;
+                case "i":
+                    Console.WriteLine("OK");
+                    break;
+                case "I":
+                    Console.WriteLine("OK");
+                    break;
+                case "o":
+                    Console.WriteLine("OK");
+                    break;
+                case "O":
+                    Console.WriteLine("OK");
+                    break;
+                case "u":
+                    Console.WriteLine("OK");
+                    break;
+                case "U":
+                    Console.WriteLine("OK");
+                    break;
+                default:
+                    Console.WriteLine("ERROR!");
+                    break;
+            }
+        }
+    }
+}
---
+++ GetVowel/Sum/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter A Number: ");
+            bool Valid = true;
+            while (Valid == true)
+            {
+                float num = Convert.ToInt32(Console.ReadLine());
+                float sum = num += num;
+                Console.WriteLine(sum);
+                if (num == 999)
+                {
+                    Valid = false;
+                }
+            }
+
+        }
+    }
+}
