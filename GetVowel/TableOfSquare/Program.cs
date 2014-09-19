9ded3f4358ced1ed155d57e98a53034982539d75
spencerj <vorpehd@gmail.com>
2014-09-19 08:41:38 -0400

    .
    ..

--- GetVowel/TableOfSquare/Program.cs
+++ GetVowel/TableOfSquare/Program.cs
@@ -9,6 +9,19 @@ namespace TableOfSquare
     {
         static void Main(string[] args)
         {
+            int i = Convert.ToInt32(Console.ReadLine());
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            while (i <= 50)
+            {
+                int sum = i + a;
+                Console.WriteLine("\n" + sum + "\n");
+                break;
+            }
+            while (a <= 50)
+            {
+                break;
+            }
         }
     }
 }
